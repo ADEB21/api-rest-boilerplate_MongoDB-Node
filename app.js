46c24ee00596253f122d63cb4838d7a384ef6eaf
ADEB <elliurbedruhtra@gmail.com>
2023-11-03 17:37:04 +0100

    🔧 delete things

--- app.js
+++ app.js
@@ -99,19 +99,4 @@ app.use("/hello", (req, res, next) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
-});
-
 module.exports = app;
